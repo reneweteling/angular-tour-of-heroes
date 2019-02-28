c1598373ef917d5818016321f09723c43d0dcec0
Rene Weteling <rene@weteling.com>
2019-02-28 14:58:00 +0100

    Added router

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-herores';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) {}
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
